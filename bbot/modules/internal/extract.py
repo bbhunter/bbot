f72315fda447c059bc9ecb015e349adca0cc0933
Dom Whewell <dom.whewell@sage.com>
2024-11-12 10:09:36 +0000

    Add a map of the different compression types, comment them until a test has been written

--- bbot/modules/internal/extract.py
+++ bbot/modules/internal/extract.py
@@ -1,7 +1,16 @@
 import zipfile
 
+# import bz2
+# import lzma
+# import expak
+# import tarfile
+# import rarfile
+# import py7zr
+# import zstandard as zstd
+# import lz4.frame
+# import shutil
+
 from pathlib import Path
-from subprocess import CalledProcessError
 from bbot.modules.internal.base import BaseInternalModule
 
 
@@ -14,80 +23,62 @@ class extract(BaseInternalModule):
         "created_date": "2024-11-04",
         "author": "@domwhewell-sage",
     }
-    options = {
-        "threads": 4,
-    }
-    options_desc = {
-        "threads": "Maximum jadx threads for extracting apk's, default: 4",
-    }
-    deps_ansible = [
-        {
-            "name": "Install latest JRE (Debian)",
-            "package": {"name": ["default-jre"], "state": "present"},
-            "become": True,
-            "when": "ansible_facts['os_family'] == 'Debian'",
-        },
-        {
-            "name": "Install latest JRE (Arch)",
-            "package": {"name": ["jre-openjdk"], "state": "present"},
-            "become": True,
-            "when": "ansible_facts['os_family'] == 'Archlinux'",
-        },
-        {
-            "name": "Install latest JRE (Fedora)",
-            "package": {"name": ["java-openjdk-headless"], "state": "present"},
-            "become": True,
-            "when": "ansible_facts['os_family'] == 'RedHat'",
-        },
-        {
-            "name": "Install latest JRE (Alpine)",
-            "package": {"name": ["openjdk11"], "state": "present"},
-            "become": True,
-            "when": "ansible_facts['os_family'] == 'Alpine'",
-        },
-        {
-            "name": "Create jadx directory",
-            "file": {"path": "#{BBOT_TOOLS}/jadx", "state": "directory", "mode": "0755"},
-        },
-        {
-            "name": "Download jadx",
-            "unarchive": {
-                "src": "https://github.com/skylot/jadx/releases/download/v1.5.0/jadx-1.5.0.zip",
-                "include": ["lib/jadx-1.5.0-all.jar", "bin/jadx"],
-                "dest": "#{BBOT_TOOLS}/jadx",
-                "remote_src": True,
-            },
-        },
-    ]
-
-    zipcompressed = ["doc", "dot", "docm", "docx", "ppt", "pptm", "pptx", "xls", "xlt", "xlsm", "xlsx", "zip"]
-    jadx = ["xapk", "apk"]
-    allowed_extensions = zipcompressed + jadx
+    # deps_pip = ["rarfile", "py7zr", "zstandard", "lz4"]
 
     async def setup(self):
-        self.threads = self.config.get("threads", 4)
+        self.compression_methods = {
+            "zip": self.extract_zip_file,
+            #     "bz2": lambda path, output_dir: self.extract_bz2_file(path, output_dir / "content.txt"),
+            #     "xz": lambda path, output_dir: self.extract_xz_file(path, output_dir / "content.txt"),
+            #     "7z": self.extract_7z_file,
+            #     "rar": self.extract_rar_file,
+            #     "lzma": lambda path, output_dir: self.extract_lzma_file(path, output_dir / "content.txt"),
+            #     "compress": lambda path, output_dir: self.extract_compress_file(path, output_dir / "content.txt"),
+            #     "zstd": lambda path, output_dir: self.extract_zstd_file(path, output_dir / "content.txt"),
+            #     "lz4": lambda path, output_dir: self.extract_lz4_file(path, output_dir / "content.txt"),
+            #     "tar": self.extract_tar_file,
+            #     "pak": self.extract_pak_file,
+            #     "lha": self.extract_lha_file,
+            #     "arj": self.extract_arj_file,
+            #     "cab": self.extract_cab_file,
+            #     "sit": self.extract_sit_file,
+            #     "binhex": lambda path, output_dir: self.extract_binhex_file(path, output_dir / "content.txt"),
+            #     "lrzip": lambda path, output_dir: self.extract_lrzip_file(path, output_dir / "content.txt"),
+            #     "alz": self.extract_alz_file,
+            #     "tgz": self.extract_tgz_file,
+            #     "gzip": lambda path, output_dir: self.extract_gzip_file(path, output_dir / "content.txt"),
+            #     "lzip": lambda path, output_dir: self.extract_lzip_file(path, output_dir / "content.txt"),
+            #     "palm": lambda path, output_dir: self.extract_palm_file(path, output_dir / "content.txt"),
+            #     "cpio": self.extract_cpio_file,
+            #     "pack200": lambda path, output_dir: self.extract_pack200_file(path, output_dir / "content.txt"),
+            #     "par2": lambda path, output_dir: self.extract_par2_file(path, output_dir / "content.txt"),
+            #     "ar": self.extract_ar_file,
+            #     "qpress": self.extract_qpress_file,
+            #     "xar": self.extract_xar_file,
+            #     "ace": self.extract_ace_file,
+            #     "zoo": self.extract_zoo_file,
+            #     "arc": self.extract_arc_file,
+        }
         return True
 
     async def filter_event(self, event):
         if "file" in event.tags:
-            if not any(event.data["path"].endswith(f".{ext}") for ext in self.allowed_extensions):
+            if not event.data["compression"] in self.compression_methods:
                 return False, "Extract unable to handle file type"
         else:
             return False, "Event is not a file"
         return True
 
     async def handle_event(self, event):
+        compression_format = event.data["compression"]
         path = Path(event.data["path"])
-        extension = path.suffix.strip(".").lower()
         output_dir = path.parent / path.name.replace(".", "_")
         self.helpers.mkdir(output_dir)
 
         # Use the appropriate extraction method based on the file type
         self.info(f"Extracting {path} to {output_dir}")
-        if extension in self.zipcompressed:
-            success = self.extract_zip_file(path, output_dir)
-        elif extension in self.jadx:
-            success = await self.decompile_apk(path, output_dir)
+        extract_method = self.compression_methods.get(compression_format)
+        success = extract_method(path, output_dir)
 
         # If the extraction was successful, emit the event
         if success:
@@ -110,22 +101,288 @@ class extract(BaseInternalModule):
             return False
         return True
 
-    async def decompile_apk(self, path, output_dir):
-        command = [
-            f"{self.scan.helpers.tools_dir}/jadx/bin/jadx",
-            "--threads-count",
-            self.threads,
-            "--output-dir",
-            str(output_dir),
-            str(path),
-        ]
-        try:
-            output = await self.run_process(command, check=True)
-        except CalledProcessError as e:
-            self.warning(f"Error decompiling {path}. STDERR: {repr(e.stderr)}")
-            return False
-        if not Path(output_dir / "resources").exists() and not Path(output_dir / "sources").exists():
-            self.warning(f"JADX was unable to decompile {path}.")
-            self.warning(output)
-            return False
-        return True
+    # def extract_bz2_file(self, path, output_file):
+    #     try:
+    #         with bz2.BZ2File(path, "rb") as file:
+    #             content = file.read()
+    #             with open(output_file, "wb") as f:
+    #                 f.write(content)
+    #     except Exception as e:
+    #         self.warning(f"Error extracting {path}. Exception: {repr(e)}")
+    #         return False
+    #     return True
+
+
+#
+# def extract_xz_file(self, path, output_file):
+#     try:
+#         with lzma.open(path, "rb") as file:
+#             content = file.read()
+#             with open(output_file, "wb") as f:
+#                 f.write(content)
+#     except Exception as e:
+#         self.warning(f"Error extracting {path}. Exception: {repr(e)}")
+#         return False
+#     return True
+#
+# def extract_7z_file(self, path, output_dir):
+#     try:
+#         with py7zr.SevenZipFile(path, mode="r") as z:
+#             z.extractall(path=output_dir)
+#     except Exception as e:
+#         self.warning(f"Error extracting {path}. Exception: {repr(e)}")
+#         return False
+#     return True
+#
+# def extract_rar_file(self, path, output_dir):
+#     try:
+#         with rarfile.RarFile(path, "r") as rar_ref:
+#             rar_ref.extractall(output_dir)
+#     except Exception as e:
+#         self.warning(f"Error extracting {path}. Exception: {repr(e)}")
+#         return False
+#     return True
+#
+# def extract_lzma_file(self, path, output_file):
+#     try:
+#         with lzma.open(path, "rb") as file:
+#             content = file.read()
+#             with open(output_file, "wb") as f:
+#                 f.write(content)
+#     except Exception as e:
+#         self.warning(f"Error extracting {path}. Exception: {repr(e)}")
+#         return False
+#     return True
+#
+# def extract_compress_file(self, path, output_file):
+#     try:
+#         with open(path, "rb") as file:
+#             content = file.read()
+#             with open(output_file, "wb") as f:
+#                 f.write(content)
+#     except Exception as e:
+#         self.warning(f"Error extracting {path}. Exception: {repr(e)}")
+#         return False
+#     return True
+#
+# def extract_zstd_file(self, path, output_file):
+#     try:
+#         with open(path, "rb") as file:
+#             dctx = zstd.ZstdDecompressor()
+#             content = dctx.decompress(file.read())
+#             with open(output_file, "wb") as f:
+#                 f.write(content)
+#     except Exception as e:
+#         self.warning(f"Error extracting {path}. Exception: {repr(e)}")
+#         return False
+#     return True
+#
+# def extract_lz4_file(self, path, output_file):
+#     try:
+#         with lz4.frame.open(path, "rb") as file:
+#             content = file.read()
+#             with open(output_file, "wb") as f:
+#                 f.write(content)
+#     except Exception as e:
+#         self.warning(f"Error extracting {path}. Exception: {repr(e)}")
+#         return False
+#     return True
+#
+# def extract_tar_file(self, path, output_dir):
+#     try:
+#         with tarfile.open(path, "r") as tar_ref:
+#             tar_ref.extractall(output_dir)
+#     except Exception as e:
+#         self.warning(f"Error extracting {path}. Exception: {repr(e)}")
+#         return False
+#     return True
+#
+# def extract_pak_file(self, path, output_dir):
+#     try:
+#         expak.extract_resources(path, output_dir)
+#     except Exception as e:
+#         self.warning(f"Error extracting {path}. Exception: {repr(e)}")
+#         return False
+#     return True
+#
+# def extract_lha_file(self, path, output_dir):
+#     try:
+#         shutil.unpack_archive(path, output_dir)
+#     except Exception as e:
+#         self.warning(f"Error extracting {path}. Exception: {repr(e)}")
+#         return False
+#     return True
+#
+# def extract_arj_file(self, path, output_dir):
+#     try:
+#         shutil.unpack_archive(path, output_dir)
+#     except Exception as e:
+#         self.warning(f"Error extracting {path}. Exception: {repr(e)}")
+#         return False
+#     return True
+#
+# def extract_cab_file(self, path, output_dir):
+#     try:
+#         shutil.unpack_archive(path, output_dir)
+#     except Exception as e:
+#         self.warning(f"Error extracting {path}. Exception: {repr(e)}")
+#         return False
+#     return True
+#
+# def extract_sit_file(self, path, output_dir):
+#     try:
+#         shutil.unpack_archive(path, output_dir)
+#     except Exception as e:
+#         self.warning(f"Error extracting {path}. Exception: {repr(e)}")
+#         return False
+#     return True
+#
+# def extract_binhex_file(self, path, output_file):
+#     try:
+#         with open(path, "rb") as file:
+#             content = file.read()
+#             with open(output_file, "wb") as f:
+#                 f.write(content)
+#     except Exception as e:
+#         self.warning(f"Error extracting {path}. Exception: {repr(e)}")
+#         return False
+#     return True
+#
+# def extract_lrzip_file(self, path, output_file):
+#     try:
+#         with open(path, "rb") as file:
+#             content = file.read()
+#             with open(output_file, "wb") as f:
+#                 f.write(content)
+#     except Exception as e:
+#         self.warning(f"Error extracting {path}. Exception: {repr(e)}")
+#         return False
+#     return True
+#
+# def extract_alz_file(self, path, output_dir):
+#     try:
+#         shutil.unpack_archive(path, output_dir)
+#     except Exception as e:
+#         self.warning(f"Error extracting {path}. Exception: {repr(e)}")
+#         return False
+#     return True
+#
+# def extract_tgz_file(self, path, output_dir):
+#     try:
+#         with tarfile.open(path, "r:gz") as tar_ref:
+#             tar_ref.extractall(output_dir)
+#     except Exception as e:
+#         self.warning(f"Error extracting {path}. Exception: {repr(e)}")
+#         return False
+#     return True
+#
+# def extract_gzip_file(self, path, output_file):
+#     try:
+#         with open(path, "rb") as file:
+#             content = file.read()
+#             with open(output_file, "wb") as f:
+#                 f.write(content)
+#     except Exception as e:
+#         self.warning(f"Error extracting {path}. Exception: {repr(e)}")
+#         return False
+#     return True
+#
+# def extract_lzip_file(self, path, output_file):
+#     try:
+#         with open(path, "rb") as file:
+#             content = file.read()
+#             with open(output_file, "wb") as f:
+#                 f.write(content)
+#     except Exception as e:
+#         self.warning(f"Error extracting {path}. Exception: {repr(e)}")
+#         return False
+#     return True
+#
+# def extract_palm_file(self, path, output_file):
+#     try:
+#         with open(path, "rb") as file:
+#             content = file.read()
+#             with open(output_file, "wb") as f:
+#                 f.write(content)
+#     except Exception as e:
+#         self.warning(f"Error extracting {path}. Exception: {repr(e)}")
+#         return False
+#     return True
+#
+# def extract_cpio_file(self, path, output_dir):
+#     try:
+#         shutil.unpack_archive(path, output_dir)
+#     except Exception as e:
+#         self.warning(f"Error extracting {path}. Exception: {repr(e)}")
+#         return False
+#     return True
+#
+# def extract_pack200_file(self, path, output_file):
+#     try:
+#         with open(path, "rb") as file:
+#             content = file.read()
+#             with open(output_file, "wb") as f:
+#                 f.write(content)
+#     except Exception as e:
+#         self.warning(f"Error extracting {path}. Exception: {repr(e)}")
+#         return False
+#     return True
+#
+# def extract_par2_file(self, path, output_file):
+#     try:
+#         with open(path, "rb") as file:
+#             content = file.read()
+#             with open(output_file, "wb") as f:
+#                 f.write(content)
+#     except Exception as e:
+#         self.warning(f"Error extracting {path}. Exception: {repr(e)}")
+#         return False
+#     return True
+#
+# def extract_ar_file(self, path, output_dir):
+#     try:
+#         shutil.unpack_archive(path, output_dir)
+#     except Exception as e:
+#         self.warning(f"Error extracting {path}. Exception: {repr(e)}")
+#         return False
+#     return True
+#
+# def extract_qpress_file(self, path, output_dir):
+#     try:
+#         shutil.unpack_archive(path, output_dir)
+#     except Exception as e:
+#         self.warning(f"Error extracting {path}. Exception: {repr(e)}")
+#         return False
+#     return True
+#
+# def extract_xar_file(self, path, output_dir):
+#     try:
+#         shutil.unpack_archive(path, output_dir)
+#     except Exception as e:
+#         self.warning(f"Error extracting {path}. Exception: {repr(e)}")
+#         return False
+#     return True
+#
+# def extract_ace_file(self, path, output_dir):
+#     try:
+#         shutil.unpack_archive(path, output_dir)
+#     except Exception as e:
+#         self.warning(f"Error extracting {path}. Exception: {repr(e)}")
+#         return False
+#     return True
+#
+# def extract_zoo_file(self, path, output_dir):
+#     try:
+#         shutil.unpack_archive(path, output_dir)
+#     except Exception as e:
+#         self.warning(f"Error extracting {path}. Exception: {repr(e)}")
+#         return False
+#     return True
+#
+# def extract_arc_file(self, path, output_dir):
+#     try:
+#         shutil.unpack_archive(path, output_dir)
+#     except Exception as e:
+#         self.warning(f"Error extracting {path}. Exception: {repr(e)}")
+#         return False
+#     return True
--- bbot/test/test_step_2/module_tests/test_module_extract.py
+++ bbot/test/test_step_2/module_tests/test_module_extract.py
@@ -1,7 +1,7 @@
 import zipfile
 
 from pathlib import Path
-from .base import ModuleTestBase, tempapkfile
+from .base import ModuleTestBase
 
 
 class TestExtractZip(ModuleTestBase):
@@ -39,51 +39,3 @@ class TestExtractZip(ModuleTestBase):
         assert 1 == len(extract_event), "Failed to extract zip"
         extract_path = Path(extract_event[0].data["path"])
         assert extract_path.is_dir(), "Destination folder doesn't exist"
-
-
-class TestExtractApk(ModuleTestBase):
-    modules_overrides = ["apkpure", "google_playstore", "speculate", "extract"]
-    apk_file = tempapkfile()
-
-    async def setup_after_prep(self, module_test):
-        await module_test.mock_dns({"blacklanternsecurity.com": {"A": ["127.0.0.99"]}})
-        module_test.httpx_mock.add_response(
-            url="https://play.google.com/store/search?q=blacklanternsecurity&c=apps",
-            text="""<!DOCTYPE html>
-            <html>
-            <head>
-            <title>"blacklanternsecurity" - Android Apps on Google Play</title>
-            </head>
-            <body>
-            <a href="/store/apps/details?id=com.bbot.test&pcampaignid=dontmatchme&pli=1"/>
-            </body>
-            </html>""",
-        )
-        module_test.httpx_mock.add_response(
-            url="https://play.google.com/store/apps/details?id=com.bbot.test",
-            text="""<!DOCTYPE html>
-            <html>
-            <head>
-            <title>BBOT</title>
-            </head>
-            <body>
-            <meta name="appstore:developer_url" content="https://www.blacklanternsecurity.com">
-            </div>
-            </div>
-            </body>
-            </html>""",
-        )
-        module_test.httpx_mock.add_response(
-            url="https://d.apkpure.com/b/XAPK/com.bbot.test?version=latest",
-            content=self.apk_file,
-        )
-
-    def check(self, module_test, events):
-        extract_event = [
-            e
-            for e in events
-            if e.type == "FILESYSTEM" and "com_bbot_test_xapk" in e.data["path"] and "folder" in e.tags
-        ]
-        assert 1 == len(extract_event), "Failed to extract apk"
-        extract_path = Path(extract_event[0].data["path"])
-        assert extract_path.is_dir(), "Destination apk doesn't exist"
