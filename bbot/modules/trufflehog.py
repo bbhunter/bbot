a7b8c3115a2f3afa4d0fd1a3a0da49bf9be7c229
blsaccess <info@blacklanternsecurity.com>
2024-12-19 00:24:02 +0000

    Update trufflehog

--- bbot/modules/trufflehog.py
+++ bbot/modules/trufflehog.py
@@ -13,7 +13,7 @@ class trufflehog(BaseModule):
     }
 
     options = {
-        "version": "3.87.0",
+        "version": "3.87.1",
         "config": "",
         "only_verified": True,
         "concurrency": 8,
